e80fe7d573a38acd9d9022f14969a57661fcda3f
Amarachi <amarachi32@gmail.com>
2026-02-01 23:45:18 +0100

    configure url

--- Client/src/lib/apiClient.ts
+++ Client/src/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
-
+//const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://starttech-alb-1968314094.us-east-1.elb.amazonaws.com/swagger/index.html';
 export const apiClient = axios.create({
     baseURL: API_BASE_URL,
     withCredentials: true, // Crucial for httpOnly cookies
